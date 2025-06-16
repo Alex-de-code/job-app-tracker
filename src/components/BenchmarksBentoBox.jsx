904ef3a38c0628d6b10fd56c551dd3f7aa956733
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2025-06-16 13:48:22 -0400

    styled + cleaned up alignment of pie chart

--- src/components/BenchmarksBentoBox.jsx
+++ src/components/BenchmarksBentoBox.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { GiAchievement } from "react-icons/gi";
+import { PieChart, Pie, Sector, ResponsiveContainer, Legend } from "recharts";
 
 const BenchmarksBentoBox = () => {
   // TODO: setup business logic in supabase for handling counters and sending back info so most up-to-date info is shown to the user -- & replace hard coded data with this
@@ -13,6 +14,16 @@ const BenchmarksBentoBox = () => {
   // Should I add progress bar for users to see if they've reached goal? Or is counter enough? maybe counter changes color to green once reached? or I keep badge?
 
   // Should I add a heat map like Github so users know how many weeks they reached weekly goal, or everyday they applied to jobs????
+
+  // once pie chart is live will need a condiitional rednder for when there is no data yet in system of user job apps
+
+  //DUmmy data for pie chart
+  const data = [
+    { name: "Accepted", value: 300, fill: "#43a047" },
+    { name: "Interviewing", value: 200, fill: "#fdd835" },
+    { name: "Denied", value: 500, fill: "#e53935" },
+    { name: "Applied", value: 100, fill: "#90a4ae" },
+  ];
 
   return (
     <>
@@ -38,12 +49,44 @@ const BenchmarksBentoBox = () => {
           <div className="text-gray-500 text-sm">Total Applications</div>
           <div className="text-4xl font-bold mt-2">234</div>
         </div>
-        {/* <div className="col-span-2 row-span-4"> */}
-        {/* <div className="bg-white border border-gray-200 rounded-xl p-4 shadow"> */}
-        {/* <div className="text-gray-500 text-sm">Interview Rate</div>
+        <div className="col-span-2 row-span-2 bg-slate-50 rounded-xl p-4 shadow">
+          <div className="text-gray-500 text-sm">Application Status</div>
+
+          {/* <div className="bg-white border border-gray-200 rounded-xl p-4 shadow"> */}
+          {/* <div className="text-gray-500 text-sm">Interview Rate</div>
             <div className="text-4xl font-bold text-emerald-600">32%</div> */}
-        {/* </div> */}
-        {/* </div> */}
+          {/* </div> */}
+          {/* </div> */}
+          <ResponsiveContainer width="100%" height={227}>
+            <PieChart
+              width={300}
+              height={230}
+              // margin={{ top: 10, right: 0, left: 50, bottom: 0 }}
+            >
+              <Pie
+                // activeIndex={this.state.activeIndex}
+                // activeShape={renderActiveShape}
+                data={data}
+                cx="49%"
+                cy="47%"
+                innerRadius={53}
+                outerRadius={70}
+                // fill="#8894d8"
+                // fill="ffffff"
+                dataKey="value"
+                paddingAngle={3}
+                strokeWidth={1}
+                label
+                // labelLine={false}
+              />
+              <Legend
+                layout="horizontal"
+                align="center"
+                verticalAlign="bottom"
+              />
+            </PieChart>
+          </ResponsiveContainer>
+        </div>
       </div>
     </>
   );
